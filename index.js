74b07430124545ee4e3e503738c975037cb311f2
LiraNalina <mara3575@hotmail.com>
2022-04-19 12:40:38 +0600

    Javascript Filter-1
    Task from codewars

--- index.js
+++ index.js
@@ -18,3 +18,23 @@ function ageDays(year, month, day) {
   }
   
   console.log(ageDays([2022, 3, 1]));
+
+
+  /* Javascript filter - 1
+  While developing a website, you detect that some of the members have troubles logging in. 
+  Searching through the code you find that all logins ending with a "_" make problems.
+  So you want to write a function that takes an array of pairs of login-names and e-mails,
+  and outputs an array of all login-name, e-mails-pairs from the login-names that end with "_".
+If you have the input-array:
+[ [ "foo", "foo@foo.com" ], [ "bar_", "bar@bar.com" ] ]
+it should output
+[ [ "bar_", "bar@bar.com" ] ]
+You have to use the filter-method which returns each element of the array for which the filter-method returns true.
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter
+ */
+
+function checkNames(arr){
+  return arr.filter((logins) => logins[0].match(/_$/));
+}
+
+console.log(checkNames([[ "foo", "foo@foo.com" ],["bar_", "bar@bar.com"]]));
