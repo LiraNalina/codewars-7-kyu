ddac766d5523a1045ca235add4afe588d93b7ef5
LiraNalina <mara3575@hotmail.com>
2022-04-20 01:26:27 +0600

    Added new Task from Codewars
    Training on Categorize New Members

--- index.js
+++ index.js
@@ -38,3 +38,33 @@ function checkNames(arr){
 }
 
 console.log(checkNames([[ "foo", "foo@foo.com" ], ["bar_", "bar@bar.com"]]));
+
+
+The Western Suburbs Croquet Club has two categories of membership, Senior and Open. They would like your help with an application form that will tell prospective members which category they will be placed.
+To be a senior, a member must be at least 55 years old and have a handicap greater than 7. In this croquet club, handicaps range from -2 to +26; the better the player the lower the handicap.
+Input
+Input will consist of a list of pairs. Each pair contains information for a single potential member. Information consists of an integer for the person's age and an integer for the person's handicap.
+Output
+Output will consist of a list of string values (in Haskell and C: Open or Senior) stating whether the respective member is to be placed in the senior or open category.
+Example
+input =  [[18, 20], [45, 2], [61, 12], [37, 6], [21, 21], [78, 9]]
+output = ["Open", "Open", "Senior", "Open", "Open", "Senior"
+
+function openOrSenior(data) {
+  let members = [];
+
+  for (let i = 0; i < data.length; i++) {
+    if (data[i][0] >= 55 && data[i][1] > 7 && data[i][1] <= 26) {
+      members.push("Senior");
+    } else if ( data[i][0] < 55 && data[i][1] > -2 || data[i][1] <= 7) {
+      members.push("Open");
+    }
+  }
+  return members;
+}
+
+console.log(
+  openOrSenior([
+    [27,21],[20,3],[21,20],[15,-2],[17,21],[54,-1],[19,22],[79,-2]
+  ])
+);
