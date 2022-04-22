f87993cea86c6ff86f140fe8666807d5af9fbd4c
LiraNalina <mara3575@hotmail.com>
2022-04-22 09:59:47 +0600

    Another task from codewars 8 kyu
    Needed to be in 8 kyu folder

--- index.js
+++ index.js
@@ -104,3 +104,23 @@ function unusedDig(array) {
   return parseInt(a[0], 10);
 }
 console.log(unusedDig([1, 10, 100]));
+
+
+/* Sum of All Arguments
+Write a function that finds the sum of all its arguments.
+
+eg:
+
+sum(1, 2, 3) // => 6
+sum(8, 2) // => 10
+sum(1, 2, 3, 4, 5) // => 15 */
+
+function sumArgs(...args) {
+  let sum = 0;
+  for (let i = 0; i < arguments.length; i++) {
+    sum = sum + arguments[i];
+  }
+  return sum;
+}
+
+console.log(sumArgs(1, 2, 4, 5));
